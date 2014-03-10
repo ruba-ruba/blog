fc53606fed344e1ef95261f4b3951d901fee2ac7
ruba-ruba <gunit1991@ukr.net>
2014-03-10 14:22:26 +0200

    change search position

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include ActionView::Helpers::DateHelper
   include HubsHelper
+  
   before_filter :hubs
+  
   helper_method :render_menu_partial
-
+  helper_method :post_show
+  helper_method :created
   
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -21,6 +25,14 @@ class ApplicationController < ActionController::Base
 
   def render_menu_partial
     %w(posts hubs).include?(controller_name) && %w(index show).include?(action_name) 
+  end
+
+  def post_show
+    "#{controller_name}/#{action_name}" == 'posts/show'
+  end
+
+  def created object
+    "#{time_ago_in_words(object.created_at)} ago" 
   end
   
 end
