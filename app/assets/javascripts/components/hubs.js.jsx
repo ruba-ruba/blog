8adb3caee490292021a58cd9ef8d6bdca5bb95dc
ruba-ruba <gunit1991@ukr.net>
2015-05-11 16:29:14 +0300

    render Posts as react components

---
+++ app/assets/javascripts/components/hubs.js.jsx
@@ -0,0 +1,26 @@
+var Hub = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    link: React.PropTypes.string
+  },
+
+  render: function() {
+    return (
+      <div className='hub'>
+        <a href={this.props.link}>
+          <i className='fi-pencil'></i>
+          &nbsp;
+          {this.props.title}
+        </a>
+      </div>
+    );
+  }
+});
+
+var HubTitlePrefix = React.createClass({
+  render: function () {
+    return (
+       <div className='hub'>Categories:</div>
+    );
+  }
+});
