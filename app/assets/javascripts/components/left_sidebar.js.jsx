27967eb0b2f5767af6a3960559ed0aefc953fca4
ruba_ruba <gunit1991@ukr.net>
2015-07-19 18:52:22 +0300

    communication between components

--- app/assets/javascripts/components/left_sidebar.js.jsx
+++ app/assets/javascripts/components/left_sidebar.js.jsx
@@ -1,10 +1,18 @@
+var APP = {};
+
 var LinkToLeftSidebar = React.createClass({
     propTypes: {
       class_name: React.PropTypes.string
     },
+
+    trigger_sidebar: function() {
+      $(APP).trigger('change');
+    },
+
+
     render: function () {
         return (
-            <div>
+            <div onClick={this.trigger_sidebar}>
               <a href='#' className={this.props.class_name}> ||| </a>
             </div>
         );
@@ -17,27 +25,31 @@ var LeftSidebar = React.createClass({
     getInitialState: function(){
       return { focused: 'disabled' };
     },
+    componentDidMount: function(){
+      $(APP).on('change', function(e){
+        if(this.state.focused == 'active'){
+          this.setState({focused: 'disabled'})
+        } else {
+          this.setState({focused: 'active'})
+        }
+      }.bind(this));
+    },
+    componentWillUnmount: function () {
+      $(APP).off('change');
+    },
 
     clicked: function(index){
-      // The click handler will update the state with
-      // the index of the focused menu entry
+        console.log(this.state.focused);
       this.setState({focused: index});
     },
 
-
     render: function () {
         var self = this;
-        if(self.state.focused == 'active'){
-          style = 'disabled';
-          self.state.focused = 'disabled'
-        } else {
-          style = 'active';
-          self.state.focused = 'active'
-        }
+        var style = this.state.focused;
 
         return (
             <div className={style} onClick={self.clicked.bind(self, self.state.focused)}>
-              LEFT SIDE MENU
+              LEFT SIDE MENUs
             </div>
         );
     }
